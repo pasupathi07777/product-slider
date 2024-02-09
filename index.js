23a6115c3baaf5c4193c287d0b06425fbc6e373f
pasupathi07777 <pasupathi07777@gamil.com>
2024-02-09 07:04:42 -0800

    jj

--- index.js
+++ index.js
@@ -3,40 +3,19 @@ let img_btn = box.querySelectorAll(".img-box")
 let play_box = document.querySelector(".box-1")
 let box_1 = document.querySelector(".box-1")
 let box_1_image = box_1.querySelectorAll("img")
-// console.log(box_1_image)
+
 let wid;
 
 
-// init()
-// function init(){
-//     // let width=document.querySelector(".box-1 img:first-child").clientWidth
-//     // let cha_wid=document.querySelector(".box-1 img:first-child")
-//     // cha_wid.addEventListener("resize",()=>{
-//     //     console.log(width)
-
-//     // })
-//     // )
-//     window.addEventListener("resize",()=>{
-     
-//         // let width=document.querySelector(".section-1").clientWidth
-//         // let t_wid=width
-//         // console.log(t_wid,"t-width")
-//         // let size_img=document.querySelector(".box-1")
-//         // console.log(size_img.clientWidth)
-//         // size_img.style.width="100%"
-//     })
-    
-    
-
-// }
 
 let index;
 
 
 img_btn.forEach((a,i)=>{
     a.addEventListener("click",(e)=>{
+        
         index=i+1
-        // console.log(index)
+       
         updatebox(index)
     })
 })
@@ -48,6 +27,36 @@ function updatebox(index){
     width=(index-1)*width
     console.log(width)
     box_1.style.transform=`translateX(${-width}px)`; 
+
+    // let f_ind=(index==0)?0:index-1
+    let boder_int=index-1
+    
+
+    // img_btn[index-1].classList.add("img-box-active")
+    img_btn.forEach((a,i)=>{
+        if(boder_int==0){
+            img_btn[0].classList.add("img-box-active")
+            img_btn[1].classList.remove("img-box-active")
+            img_btn[2].classList.remove("img-box-active")
+            
+
+        }
+        else if(boder_int==1){
+            img_btn[1].classList.add("img-box-active")
+            img_btn[0].classList.remove("img-box-active")
+            img_btn[2].classList.remove("img-box-active")
+        2
+
+        }
+        else if(boder_int==2){
+            img_btn[2].classList.add("img-box-active")
+            img_btn[0].classList.remove("img-box-active")
+            img_btn[1].classList.remove("img-box-active")
+            
+
+        }
+    })
+
 
     
   
